90ce420ecbca1b0f174113e94b6e6baf1e9bf0ba
Matt Thornberry <mtberry16@hotmail.com>
2019-06-29 15:37:18 -0600

    Completed the steps in the unFullfilled and onRejected functions lesson

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const {checkInventory} = require('./library.js');
 
 const order = [['sunglasses', 1], ['bags', 2]];
+
+const handleSuccess = (resolvedValue) => {
+    console.log(resolvedValue);
+};
+
+const handleFailure = (rejectionReason) => {
+    console.log(rejectionReason);
+};
